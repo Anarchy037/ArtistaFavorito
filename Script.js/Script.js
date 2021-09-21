848dfc1d39b60d66200f1e46cc18de786ba90b9b
Anarchy037 <leonardocubarias@gmail.com>
2021-09-21 18:58:27 -0500

    primer commit

---
+++ Script.js/Script.js
@@ -0,0 +1,31 @@
+const nombreAlbum1 = () => {
+    let nombre = "Godzilla";
+    alert("El album se llama " + nombre);
+}
+
+const nombreAlbum2 = () => {
+    let nombre = "Recovery";
+    alert("El album se llama " + nombre);
+}
+
+const nombreAlbum3 = () => {
+    let nombre = "Kamikase";
+    alert("El album se llama " + nombre);
+}
+
+const nombreAlbum4 = () => {
+    let nombre = "Encore";
+    alert("El album se llama " + nombre);
+}
+
+const nombreAlbum5 = () =>{
+    let nombre = "Revival";
+    alert("El album se llama " + nombre);
+}
+
+const nombreAlbum6 = () => {
+    let nombre = "Infinite";
+    alert("El abum se llama " + nombre);
+}
+
+
